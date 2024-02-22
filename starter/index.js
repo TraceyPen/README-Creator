2d84cd24fed53e80f42c6580eb6e56836eaedffa
TraceyPen <traceylpender@yahoo.co.uk>
2024-02-22 10:42:49 +0000

    first commit

---
+++ starter/index.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+const path = require('path');
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+
+
+// array of questions for user
+const questions = [
+
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile('./README.md', process.argv[2], err => err ? console.error(err) : console.log("Success"));
+}
+
+// function to initialize program
+function init() {
+
+}
+
+// function call to initialize program
+init();
+
+writeToFile();
